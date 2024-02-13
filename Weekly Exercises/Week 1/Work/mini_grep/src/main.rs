ee1ce3295e700e8d4639b6163afb441605b6ec24
Dysthesis <antheoraviel@protonmail.com>
2024-02-14 09:21:55 +1100

    Try to get rid of newline stripping to fix mini_grep

--- Weekly Exercises/Week 1/Work/mini_grep/src/main.rs
+++ Weekly Exercises/Week 1/Work/mini_grep/src/main.rs
@@ -14,8 +14,8 @@ fn main() {
                     break;
                 }
                 if input.contains(pattern) {
-                    let stripped: String = input.clone().replace(char::is_whitespace, "");
-                    println!("{stripped}");
+                    // let stripped: String = input.clone().replace(char::is_whitespace, "");
+                    println!("{input}");
                 }
                 input.clear();
             }
