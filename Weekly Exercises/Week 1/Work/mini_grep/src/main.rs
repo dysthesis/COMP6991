8a9c48ed84be816c08007a8b6e0de13948aa1c5e
Dysthesis <antheoraviel@protonmail.com>
2024-02-13 20:55:19 +1100

    Finish lab1 mini_grep?

--- Weekly Exercises/Week 1/Work/mini_grep/src/main.rs
+++ Weekly Exercises/Week 1/Work/mini_grep/src/main.rs
@@ -1,10 +1,25 @@
+use std::io;
+
 fn main() {
-    let pattern_string = std::env::args()
+    let pattern_string: String = std::env::args()
         .nth(1)
         .expect("missing required command-line argument: <pattern>");
 
-    let pattern = &pattern_string;
-
-    // TODO: Replace the following with your code:
-    println!("The command-line argument is: {pattern}");
+    let pattern: &String = &pattern_string;
+    let mut input: String = String::new();
+    loop {
+        match io::stdin().read_line(&mut input) {
+            Ok(_n) => {
+                if input.is_empty() {
+                    break;
+                }
+                if input.contains(pattern) {
+                    let stripped: String = input.clone().replace(char::is_whitespace, "");
+                    println!("{stripped}");
+                }
+                input.clear();
+            }
+            Err(error) => println!("error: {error}"),
+        }
+    }
 }
