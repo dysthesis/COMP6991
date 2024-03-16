b21631c996bcef10409bc70d6a03c83b97f39515
Dysthesis <antheoraviel@protonmail.com>
2024-03-16 14:57:15 +1100

    New parser format

--- rslogo/src/parsers.rs
+++ rslogo/src/parsers.rs
@@ -1,38 +1,32 @@
-use nom::{
-    branch::alt, bytes::complete::tag, character::complete::multispace0, sequence::preceded,
-    IResult,
-};
+use nom::{branch::alt, bytes::complete::tag, IResult, Parser};
 
-/// This enum contains the list of valid Logo commands
-#[derive(Debug, PartialEq)]
-enum Command {
+#[derive(PartialEq, Debug)]
+enum Token {
     PenUp,
     PenDown,
-    Forward(f32),
-    Back(f32),
-    Left(f32),
-    Right(f32),
-    SetPenColor(f32),
-    Turn(f32),
-    SetHeading(f32),
-    SetX(f32),
-    SetY(f32),
+    Forward(i32),
+    Backward(i32),
+    Left(i32),
+    Right(i32),
 }
 
-fn pen_up(input: &str) -> IResult<&str, Command> {
-    preceded(multispace0, tag("PENUP"))(input).map(|(next_input, _)| (next_input, Command::PenUp))
-}
+type Program = Vec<Token>;
 
-fn pen_down(input: &str) -> IResult<&str, Command> {
-    preceded(multispace0, tag("PENDOWN"))(input)
-        .map(|(next_input, _)| (next_input, Command::PenDown))
-}
+fn parse_pen_state(input: &str) -> IResult<&str, Token> {
+    let (input, parsed): (&str, &str) = alt((tag("PENUP"), tag("PENDOWN"))).parse(input)?;
 
-fn command(input: &str) -> IResult<&str, Command> {
-    alt((
-        pen_up, pen_down,
-        // Add other parsers here
-    ))(input)
+    let result = match parsed {
+        "PENUP" => Token::PenUp,
+
+        "PENDOWN" => Token::PenDown,
+
+        _ => {
+            // TODO: Make this return an actual error
+            todo!()
+        }
+    };
+
+    Ok((input, result))
 }
 
 #[cfg(test)]
@@ -40,38 +34,11 @@ mod tests {
     use super::*;
 
     #[test]
-    fn valid_penup() {
-        let input: &str = "PENUP";
-        assert_eq!(pen_up(input), Ok(("", Command::PenUp)));
-    }
+    fn valid_states() {
+        let penup = "PENUP";
+        assert_eq!(parse_pen_state(penup), Ok(("", Token::PenUp)));
 
-    #[test]
-    fn valid_pendown() {
-        let input: &str = "PENDOWN";
-        assert_eq!(pen_down(input), Ok(("", Command::PenDown)));
-    }
-
-    #[test]
-    fn invalid_penup() {
-        let input: &str = "DEFINITELYNOT";
-        assert!(pen_up(input).is_err());
-    }
-
-    #[test]
-    fn invalid_pendown() {
-        let input: &str = "DEFINITELYNOT";
-        assert!(pen_down(input).is_err());
-    }
-
-    #[test]
-    fn trailing_penup() {
-        let input: &str = "PENUP extra";
-        assert_eq!(pen_up(input), Ok((" extra", Command::PenUp)));
-    }
-
-    #[test]
-    fn trailing_pendown() {
-        let input: &str = "PENDOWN extra";
-        assert_eq!(pen_down(input), Ok((" extra", Command::PenDown)));
+        let pendown = "PENDOWN";
+        assert_eq!(parse_pen_state(pendown), Ok(("", Token::PenDown)));
     }
 }
