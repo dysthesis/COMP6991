255056198e32699aeffc2ef9f265170de0bc88ab
Dysthesis <antheoraviel@protonmail.com>
2024-04-04 15:55:32 +1100

    Initial implementation of the Spreadsheet struct for rsheet

--- rsheets/src/lib.rs
+++ rsheets/src/lib.rs
@@ -1,18 +1,44 @@
+use rsheet_lib::cell_value::CellValue;
+use rsheet_lib::command_runner::CommandRunner;
 use rsheet_lib::connect::{Manager, Reader, Writer};
 use rsheet_lib::replies::Reply;
 
+use std::collections::HashMap;
 use std::error::Error;
 
 use log::info;
+
+/// A struct which encapsulates the Spreadsheet itself.
+struct Spreadsheet {
+    /// A hashmap which stores all of the values in the spreadsheet.
+    /// Consists of a key, a String, which represents the cell number,
+    /// and a value, the corresponding CellValue
+    values: HashMap<String, CellValue>,
+
+    /// A hashmap which maps a cell number to its corresponding cell command.
+    /// Unlike with `values`, not all cells with existing values will have a corresponding
+    /// command.
+    commands: HashMap<String, CommandRunner>,
+}
+
+impl Spreadsheet {
+    fn new() -> Self {
+        Spreadsheet {
+            values: HashMap::new(),
+            commands: HashMap::new(),
+        }
+    }
+}
 
 pub fn start_server<M>(mut manager: M) -> Result<(), Box<dyn Error>>
 where
     M: Manager,
 {
     let (mut recv, mut send) = manager.accept_new_connection().unwrap();
+    let spreadsheet: Spreadsheet = Spreadsheet::new();
     loop {
         info!("Just got message");
-        let msg = recv.read_message()?;
+        let msg: String = recv.read_message()?;
         send.write_message(Reply::Error(format!("{msg:?}")))?;
     }
 }
