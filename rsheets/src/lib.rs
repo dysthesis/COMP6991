2c9365183fc0c88b4b7c0d0d874c0262b1e0455d
Dysthesis <antheoraviel@protonmail.com>
2024-04-04 18:16:07 +1100

    Initial solution for part 1
    Error handling isn't done yet, so deal with that later.

--- rsheets/src/lib.rs
+++ rsheets/src/lib.rs
@@ -1,10 +1,11 @@
 use rsheet_lib::cell_value::CellValue;
-use rsheet_lib::command_runner::CommandRunner;
+use rsheet_lib::command_runner::{CellArgument, CommandRunner};
 use rsheet_lib::connect::{Manager, Reader, Writer};
 use rsheet_lib::replies::Reply;
 
 use std::collections::HashMap;
 use std::error::Error;
+use std::ops::Deref;
 
 use log::info;
 
@@ -16,8 +17,6 @@ struct Spreadsheet {
     values: HashMap<String, CellValue>,
 
     /// A hashmap which maps a cell number to its corresponding cell command.
-    /// Unlike with `values`, not all cells with existing values will have a corresponding
-    /// command.
     commands: HashMap<String, CommandRunner>,
 }
 
@@ -35,10 +34,55 @@ where
     M: Manager,
 {
     let (mut recv, mut send) = manager.accept_new_connection().unwrap();
-    let spreadsheet: Spreadsheet = Spreadsheet::new();
+    let mut spreadsheet: Spreadsheet = Spreadsheet::new();
     loop {
         info!("Just got message");
         let msg: String = recv.read_message()?;
-        send.write_message(Reply::Error(format!("{msg:?}")))?;
+        let commands: Vec<&str> = msg.split_whitespace().collect::<Vec<&str>>();
+
+        let _result = match commands.first() {
+            Some(verb) => match *verb {
+                "get" => {
+                    let cell: &str = match commands.get(1) {
+                        Some(val) => *val,
+                        None => todo!("make this error out"),
+                    };
+
+                    let value: CellValue = spreadsheet
+                        .values
+                        .get(cell)
+                        .expect("placeholder for an actual error")
+                        .clone();
+                    send.write_message(Reply::Value(cell.to_string(), value))
+                }
+                "set" => {
+                    let cell: &str = match commands.get(1) {
+                        Some(val) => *val,
+                        None => todo!("make this error out"),
+                    };
+
+                    spreadsheet.commands.insert(
+                        cell.into(),
+                        CommandRunner::new(commands[2..].join(" ").as_str()),
+                    );
+
+                    let command = CommandRunner::new(commands[2..].join(" ").as_str());
+                    let referenced: Vec<String> = command.find_variables();
+                    let variables: HashMap<String, CellArgument> =
+                        referenced.iter().fold(HashMap::new(), |mut acc, key| {
+                            if let Some(&ref value) = spreadsheet.values.get(key) {
+                                acc.insert(key.into(), CellArgument::Value(value.clone()));
+                            }
+                            acc
+                        });
+                    spreadsheet
+                        .values
+                        .insert(cell.into(), command.run(&variables));
+                    Ok(())
+                }
+                _ => todo!("make this error out"),
+            },
+            None => todo!("make this error out"),
+        };
     }
 }
