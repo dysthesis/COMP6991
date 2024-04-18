2ba1c7a7b929e8e4acc6e1646c054a0ed8d23fa2
Dysthesis <antheoraviel@protonmail.com>
2024-04-18 13:21:28 +1000

    Initial implementation of w9/myrc

--- weekly/w9/myrc/src/lib.rs
+++ weekly/w9/myrc/src/lib.rs
@@ -2,11 +2,11 @@ use std::ops::Deref;
 
 struct Inner<T> {
     refcount: usize,
-    data: T
+    data: T,
 }
 
 pub struct MyRc<T> {
-    inner: *mut Inner<T>
+    inner: *mut Inner<T>,
 }
 
 impl<T> MyRc<T> {
@@ -14,7 +14,13 @@ impl<T> MyRc<T> {
         // TODO: Create a MyRc. You will need to:
         //  - use Box::into_raw to create an Inner
         //  - set refcount to an appropriate value.
-        todo!()
+        let inner = Box::new(Inner {
+            refcount: 1,
+            data: value,
+        });
+        MyRc {
+            inner: Box::into_raw(inner),
+        }
     }
 }
 
@@ -23,7 +29,10 @@ impl<T> Clone for MyRc<T> {
         // TODO: Increment the refcount,
         // and return another MyRc<T> by copying the
         // inner struct of this MyRc.
-        todo!()
+        unsafe {
+            (*self.inner).refcount += 1;
+        }
+        MyRc { inner: self.inner }
     }
 }
 
@@ -32,7 +41,13 @@ impl<T> Drop for MyRc<T> {
         // TODO: Decrement the refcount..
         // If it's 0, drop the Rc. You will need to use
         // Box::from_raw to do this.
-        todo!()
+        unsafe {
+            let inner = self.inner;
+            (*inner).refcount -= 1;
+            if (*inner).refcount == 0 {
+                let _ = Box::from_raw(inner); // Automatically dropped
+            }
+        }
     }
 }
 
@@ -41,6 +56,6 @@ impl<T> Deref for MyRc<T> {
 
     fn deref(&self) -> &T {
         // TODO: Return a &T.
-        todo!()
+        unsafe { &(*self.inner).data }
     }
 }
